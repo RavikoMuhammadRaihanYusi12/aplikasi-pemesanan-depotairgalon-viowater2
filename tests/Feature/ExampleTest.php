dcdb0e34171748ee5bbcd979cdd88f00b89c7e11
ravikomuhammad <101442920+RavikoMuhammadRaihanYusi12@users.noreply.github.com>
2026-06-15 21:54:54 +0700

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,14 +2,13 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase; // 1. Import trait ini
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
+    use RefreshDatabase; // 2. Gunakan trait ini di dalam kelas
+
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
